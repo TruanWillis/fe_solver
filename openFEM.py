3a2f99c92dcd5cc9498d4479b57fe4d5cf4c6235
koodibaas <truan.j.willis@gmail.com>
2023-02-16 21:04:45 +0000

    global K matrix basics added

--- openFEM.py
+++ openFEM.py
@@ -19,13 +19,13 @@ class cst_element:
 
         self.A = np.linalg.det(a) * 0.5
 
-        b = np.array([
+        self.b = np.array([
             [b1, 0, b2, 0, b3, 0],
             [0, c1, 0, c2, 0, c3],
             [c1, b1, c2, b2, c3, b3]
         ])
 
-        self.B = b * (1/(2*self.A))
+        self.B = self.b * (1/(2*self.A))
 
         d = np.array([
             [1, v, 0],
@@ -36,9 +36,13 @@ class cst_element:
         self.D = d * (E/(1-v**2))
 
         Bt = self.B.transpose()
-        k = np.matmul(Bt, self.D)
-        self.K = np.matmul(k, self.B)
+        #k = np.matmul(Bt, self.D)
+        #self.K = np.matmul(k, self.B)
+        #self.K = self.K * self.A * t
+        k = np.matmul(self.D, self.B)
+        self.K = np.matmul(Bt, k)
         self.K = self.K * self.A * t
+        
         
         self.K_pos = np.zeros((6, 6), dtype=object)
         count_1 = 0
@@ -51,6 +55,21 @@ class cst_element:
                 self.K_pos[count_1 + 1][count_2 + 1] = str(n1) + str(n2)
                 count_2 += 2
             count_1 += 2
+
+        self.gK_pos = np.zeros((6, 6), dtype=object)
+        count_1 = 0
+        for n1 in node_list:
+            count_2 = 0
+            for n2 in node_list:
+                origin_row = (n1*2)-2
+                origin_col = (n2*2)-2
+                self.gK_pos[count_1][count_2] = str(origin_row) + '|' + str(origin_col)
+                self.gK_pos[count_1][count_2 + 1] = str(origin_row) + '|' + str(origin_col+1)
+                self.gK_pos[count_1 + 1][count_2] = str(origin_row+1) + '|' + str(origin_col)
+                self.gK_pos[count_1 + 1][count_2 + 1] = str(origin_row+1) + '|' + str(origin_col+1)
+                count_2 += 2
+            count_1 += 2
+        
         '''
         self.K_pos = np.zeros((6, 6), dtype=object)
         for n1 in node_list:
@@ -89,8 +108,12 @@ for e in mesh:
         mesh[e]['n']
     )
 
-    mesh[e]['K'] = element.K
-    print(element.K_pos)
+    mesh[e]['data'] = element
+    #print(element.K_pos)
+    #print(element.gK_pos)
+    print(element.K)
+    print(element.b)
+
 
 node_list = []
 for e in mesh:
@@ -99,10 +122,38 @@ for e in mesh:
             node_list.append(n)
 
 dof = len(node_list) * 2
-print(dof)
+#print(dof)
 gK = np.zeros((dof, dof))
+#print(gK)
+
+
+for e in mesh:
+    print(mesh[e]['data'].K)
+    eK = mesh[e]['data'].K
+    gK_pos = mesh[e]['data'].gK_pos 
+    for i in range(eK.shape[0]):
+        for j in range(eK.shape[1]):
+            K_value = eK[i][j]
+            K_pos = gK_pos[i][j]
+            K_pos_row = int(K_pos.split('|')[0])
+            K_pos_col = int(K_pos.split('|')[1])
+            gK[K_pos_row][K_pos_col] = K_value + gK[K_pos_row][K_pos_col]
+
+print('gK')
 print(gK)
 
-#for e in mesh:
-#    count = 0
-#    for n in mesh[e]['n']:
+u = [1, 0, 1, 1, 0, 0, 0, 0]
+for i in range(gK.shape[0]):
+    for j in range(gK.shape[1]):
+        gK[i][j] = gK[i][j] * u[i]    
+        gK[i][j] = gK[i][j] * u[j]    
+
+print('gK')  
+print(gK)
+#k_new = np.dot(gK, u)
+#k_new = np.matmul(gK, u)
+#print('K_new')
+#print(k_new)
+#f = np.array(0, 0, )
+    
+
