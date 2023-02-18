0d1fca708e93d9bba6a062cb5e5415616cd8c7e9
koodibaas <truan.j.willis@gmail.com>
2023-02-18 09:42:31 +0000

    stiffness matrix class added

--- openFEM.py
+++ openFEM.py
@@ -1,84 +1,17 @@
 import numpy as np
-import pandas as pd
+from tabulate import tabulate
 
 element = np.array([[0,0], [20,0], [0,30]])
 
 
-class cst_elelemt_pd:
-    def __init__(self, x_cord, y_cord, node_list, E, v, t):
-        b1 = y_cord[1] - y_cord[2]                                                
-        b2 = y_cord[2] - y_cord[0]
-        b3 = y_cord[0] - y_cord[1]
-        c1 = x_cord[2] - x_cord[1]
-        c2 = x_cord[0] - x_cord[2]
-        c3 = x_cord[1] - x_cord[0]
-
-        a = np.array([
-            [1, x1, y1],
-            [1, x2, y2],
-            [1, x3, y3]
-        ])
-
-        A = np.linalg.det(a) * 0.5
-
-        b = np.array([
-            [b1, 0, b2, 0, b3, 0],
-            [0, c1, 0, c2, 0, c3],
-            [c1, b1, c2, b2, c3, b3]
-        ])
-
-        B = b * (1/(2*A))
-
-        d = np.array([
-            [1, v, 0],
-            [v, 1, 0],
-            [0, 0, (1-v)/2]
-        ])
-
-        D = d * (E/(1-v**2))
-
-        Bt = B.transpose()
-        
-        k = np.matmul(D, B)
-        K = np.matmul(Bt, k)
-        K = K * A * t
-        
-        K_pos = np.zeros((6, 6), dtype=object)
-        count_1 = 0
-        for n1 in node_list:
-            count_2 = 0
-            for n2 in node_list:
-                K_pos[count_1][count_2] = str(n1) + str(n2)
-                K_pos[count_1][count_2 + 1] = str(n1) + str(n2)
-                K_pos[count_1 + 1][count_2] = str(n1) + str(n2)
-                K_pos[count_1 + 1][count_2 + 1] = str(n1) + str(n2)
-                count_2 += 2
-            count_1 += 2
-
-        gK_pos = np.zeros((6, 6), dtype=object)
-        count_1 = 0
-        for n1 in node_list:
-            count_2 = 0
-            for n2 in node_list:
-                origin_row = (n1*2)-2
-                origin_col = (n2*2)-2
-                gK_pos[count_1][count_2] = str(origin_row) + '|' + str(origin_col)
-                gK_pos[count_1][count_2 + 1] = str(origin_row) + '|' + str(origin_col+1)
-                gK_pos[count_1 + 1][count_2] = str(origin_row+1) + '|' + str(origin_col)
-                gK_pos[count_1 + 1][count_2 + 1] = str(origin_row+1) + '|' + str(origin_col+1)
-                count_2 += 2
-            count_1 += 2
-        
-
-
 class cst_element:
     def __init__(self, x_cord, y_cord, node_list, E, v, t):
-        self.x_cord = x_cord                                                
-        self.y_cord = y_cord
+        self.x_cord = [float(x) for x in x_cord]                                                
+        self.y_cord = [float(y) for y in y_cord]
         self.node_list = node_list
-        self.E = E
-        self.v = v
-        self.t = t
+        self.E = float(E)
+        self.v = float(v)
+        self.t = float(t)
         self.dof = len(node_list) * 2
         
         self.calculate_area()
@@ -159,9 +92,40 @@ class cst_element:
         
 
 class global_stiffness_matrix:
-    def __init__(self, dof):
+    def __init__(self, dof, mesh):
+        self.dof = dof
+        self.mesh = mesh
+
+        self.matrix_headers = []
+        for n in range(1, int((dof/2)+1)):
+            for displacement in ['u', 'v']:
+                self.matrix_headers.append(displacement + str(n))
+
+
+    def define(self):
         gK = np.zeros((dof, dof))
 
+        for e in self.mesh:
+            eK = mesh[e]['data'].eK
+            gK_index = mesh[e]['data'].gK_index 
+            for i in range(eK.shape[0]):
+                for j in range(eK.shape[1]):
+                    K_value = eK[i][j]
+                    K_index = gK_index[i][j]
+                    K_index_row = int(K_index.split('|')[0])
+                    K_index_col = int(K_index.split('|')[1])
+                    gK[K_index_row][K_index_col] = K_value + gK[K_index_row][K_index_col]
+
+        gKr = gK
+        matrix_headers_r = self.matrix_headers
+
+        for i in range(len(u)-1, -1, -1):
+            if u[i] == 0:
+                del matrix_headers_r[i]
+                gKr = np.delete(gKr, i, 0)
+                gKr = np.delete(gKr, i, 1)
+
+        return gKr, matrix_headers_r
 
 '''
 mesh = {
@@ -231,6 +195,20 @@ for e in mesh:
     mesh[e]['data'] = element
 
 
+node_list = []
+for e in mesh:
+    for n in mesh[e]['n']:
+        if n not in node_list:
+            node_list.append(n)
+
+dof = len(node_list) * 2
+gsm = global_stiffness_matrix(dof, mesh)
+gKr, header = gsm.define()
+#header = gsm.matrix_headers
+
+print(header)
+
+
 '''
 for e in mesh:
     print(e)
@@ -254,12 +232,7 @@ for e in mesh:
     #print(element.b)
 '''
 
-node_list = []
-for e in mesh:
-    for n in mesh[e]['n']:
-        if n not in node_list:
-            node_list.append(n)
-
+'''
 dof = len(node_list) * 2
 gK = np.zeros((dof, dof))
 
@@ -275,12 +248,7 @@ for e in mesh:
             gK[K_index_row][K_index_col] = K_value + gK[K_index_row][K_index_col]
 
 
-'''
-for i in range(gK.shape[0]):
-    for j in range(gK.shape[1]):
-        gK[i][j] = gK[i][j] * u[i]    
-        gK[i][j] = gK[i][j] * u[j]    
-'''
+
 
 gKr = gK
 
@@ -288,9 +256,9 @@ for i in range(len(u)-1, -1, -1):
     if u[i] == 0:
         gKr = np.delete(gKr, i, 0)
         gKr = np.delete(gKr, i, 1)
-
+'''
 print('gKr')  
-print(gKr)
+print(tabulate(gKr, tablefmt="grid", stralign='center', headers=header, showindex=header))
 
 r = np.linalg.solve(gKr, f)
 print(r)    
